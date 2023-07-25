bd556b3f011745505c3740fe243b4176dbe613ff
amirhossein azarafrooz <amirhosseinazarafrooz@gmail.com>
2023-07-25 18:23:28 +0330

    initial : refactor database

---
+++ app/products/page.js
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import snappmarketlogo from '../../public/assets/images/snappmarket-logo.svg'
+
+export default function Products() {
+  return (
+
+    <main className=" flex flex-row gap-5 items-center justify-center p-5 h-screen">
+       
+      <p className=' flex flex-row gap-5 items-center justify-center p-5 bg-secondary rounded-lg shadow-md hover:shadow-md w-1/2 font-iransansb font-extrabold text-3xl text-white'>
+       صفحه هر  محصول   <Image src={snappmarketlogo} width={85} height={85}></Image> </p>
+
+       
+    
+    </main>
+  )
+}
