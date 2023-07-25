157f5e2d5cdb7391c3d6fe7d2977315bf10bd3dc
amirhossein azarafrooz <amirhosseinazarafrooz@gmail.com>
2023-07-25 19:41:38 +0330

    fix: edit products component for routing

--- app/products/[productsId]/page.js
+++ app/products/[productsId]/page.js
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 import snappmarketlogo from '../../../public/assets/images/snappmarket-logo.svg'
 
-export default function ProductsDetails({params}) {
+export default function ProductsDetails() {
   return (
 
     <main className=" flex flex-row gap-5 items-center justify-center p-5 h-screen">
        
       <p className=' flex flex-row gap-5 items-center justify-center p-5 bg-secondary rounded-lg shadow-md hover:shadow-md w-1/2 font-iransansb font-extrabold text-3xl text-white'>
-       صفحه هر  محصول  {params.productsId} <Image src={snappmarketlogo} width={85} height={85}></Image> </p>
+       صفحه اولین  محصول   <Image src={snappmarketlogo} width={85} height={85}></Image> </p>
 
        
     
