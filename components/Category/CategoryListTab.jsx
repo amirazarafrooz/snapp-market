2c89e5a85845547f1c738487380337a633d112f1
Pinfinity <bahramiehsan780@gmail.com>
2023-07-31 16:32:33 +0330

    feat/fix : Category Page

--- components/Category/CategoryListTab.jsx
+++ components/Category/CategoryListTab.jsx
@@ -1,52 +1,27 @@
 import Link from "next/link";
 
-export const CategoryListTab = () => {
+export const CategoryListTab = ({ list, title }) => {
   return (
-    <div className=" laptop:hidden my-10 p-4 border rounded">
+    <div className=" laptop:hidden my-10 p-4 border rounded mt-8">
       <div className="border-b pb-4">
         <p className="inline-block font-iransans">دسته بندی‌ها در</p>
         <p className="inline-block mr-2 font-iransans text-lg text-snp-primary">
-          لبنیات
+          {title}
         </p>
       </div>
       <div className="relative">
         <div className="pt-4 flex items-center justify-start overflow-x-auto scroll-smooth scrollbar-hide h-full">
-          <Link href={"/"}>
-            <div className="rounded-full border border-snp-primary px-4 ml-3 h-[3rem] flex justify-center items-center ">
-              <p className=" font-iransansl text-xl text-snp-primary ">نوشابه</p>
-            </div>
-          </Link>
-          <Link href={"/"}>
-            <div className="rounded-full border border-snp-primary px-4 ml-3 h-[3rem] flex justify-center items-center w-fit">
-              <p className=" font-iransansl text-xl text-snp-primary">
-                چای و دمنوش
-              </p>
-            </div>
-          </Link>
-          <Link href={"/"}>
-            <div className="rounded-full border border-snp-primary px-4 ml-3 h-[3rem] flex justify-center items-center text-justify w-fit">
-              <p className=" font-iransansl text-xl text-snp-primary w-full">
-                {" "}
-                نوشابه و چای و دمنوش
-              </p>
-            </div>
-          </Link>
-          <Link href={"/"}>
-            <div className="rounded-full border border-snp-primary px-4 ml-3 h-[3rem] flex justify-center items-center text-justify w-fit">
-              <p className=" font-iransansl text-xl text-snp-primary w-full">
-                {" "}
-                نوشابه و چای و دمنوش
-              </p>
-            </div>
-          </Link>
-          <Link href={"/"}>
-            <div className="rounded-full border border-snp-primary px-4 ml-3 h-[3rem] flex justify-center items-center text-justify w-fit">
-              <p className=" font-iransansl text-xl text-snp-primary w-full">
-                {" "}
-                نوشابه و چای و دمنوش
-              </p>
-            </div>
-          </Link>
+          {list.map((item) => {
+            return (
+              <Link href={"/"}>
+                <div className="rounded-full border border-snp-primary px-4 ml-3 h-[3rem] flex justify-center items-center  whitespace-nowrap">
+                  <p className=" font-iransansl text-xl text-snp-primary ">
+                    {item}
+                  </p>
+                </div>
+              </Link>
+            );
+          })}
         </div>
       </div>
     </div>
--- components/Category/CategoryTest.jsx
+++ components/Category/CategoryTest.jsx
@@ -4,15 +4,29 @@ import { CategoryListTab } from "./CategoryListTab";
 import { CategoryCarousel } from "./CategoryCarousel";
 
 const data = await getLocalData();
-const products = data.products;
 const category = data.categories;
 
-export const CategoryTest = ({}) => {
+export const CategoryTest = ({ categoryName }) => {
+  console.log(categoryName);
   return (
     <div className="w-full laptop:flex">
-      <CategoryListDesk />
-      <CategoryListTab />
-      <CategoryCarousel />
+      {category
+        .filter((item) => item.main === categoryName)
+        .map((item) => (
+          <CategoryListDesk list={item.subs} />
+        ))}
+      {category
+        .filter((item) => item.main === categoryName)
+        .map((item) => (
+          <CategoryListTab list={item.subs} title={item.mainFA}/>
+        ))}
+        {category.filter(item => item.main === categoryName)
+                  .map(item => {
+                    return <CategoryCarousel lists={item.subs} />
+                  })
+        
+        }
+      {/* <CategoryCarousel products={products} categoryName={categoryName}/> */}
     </div>
   );
 };
