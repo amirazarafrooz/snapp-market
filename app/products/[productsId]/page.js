341398ee33bb852a3d98c4794dbfa4e40612d08c
amirhossein azarafrooz <amirhosseinazarafrooz@gmail.com>
2023-08-01 02:23:22 +0330

    fix : test3 dynamic routing

--- app/products/[productsId]/page.js
+++ app/products/[productsId]/page.js
@@ -1,7 +1,29 @@
 import Image from 'next/image'
 import snappmarketlogo from '../../../public/assets/images/snappmarket-logo.svg'
 
-export default function ProductsDetails() {
+import { getLocalData } from '@/lib/localdata'
+
+
+  const data = await getLocalData();
+  const yechizi = data.products
+
+  export async function getStaticPaths(){
+    const paths= yechizi.map((post)=>{
+      return {
+        params: {
+          productsId:`${post.id}`,
+        },
+      }
+    })
+
+    return {
+      paths,
+      fallback:false,
+    }
+  }
+
+
+export default function ProductsDetails({params}) {
   return (
 
     <main className=" flex flex-row gap-5 items-center justify-center p-5 h-screen">
@@ -9,7 +31,10 @@ export default function ProductsDetails() {
       <p className=' flex flex-row gap-5 items-center justify-center p-5 bg-snp-secondary rounded-lg shadow-md hover:shadow-md w-1/2 font-iransansb font-extrabold text-3xl text-white'>
        صفحه اولین  محصول   <Image src={snappmarketlogo} width={85} height={85}></Image> </p>
 
-       
+       <div> {params.productsId}</div>
+
+    {yechizi.filter((item)=>
+       (item.id==params.productsId)).map((item)=> (<div className='bg-orange-600'>{item.name}</div>))}
     
     </main>
   )
