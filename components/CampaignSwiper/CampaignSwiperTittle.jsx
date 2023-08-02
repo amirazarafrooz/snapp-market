999aa683f8628c6ddbafdc1eab93f23ab34f0712
Pinfinity <bahramiehsan780@gmail.com>
2023-08-02 14:53:54 +0330

    edit campaignswiper for mobile

--- components/CampaignSwiper/CampaignSwiperTittle.jsx
+++ components/CampaignSwiper/CampaignSwiperTittle.jsx
@@ -1,7 +1,7 @@
 export const CampaignSwiperTittle = ({ item }) => {
   return (
     <>
-      <div className="flex w-full h-full sm:w-[20%] md:w-[10%] text-center ">
+      <div className="flex w-full h-full py-4 px-4 sm:px-0 sm:text-center sm:w-[20%] md:w-[10%] ">
         <p className="text-xl font-iransansb text-snp-white w-full">
           {item}
         </p>
