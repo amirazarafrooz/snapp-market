ccbd9dabc81b6539c3b11e4bf62b5ed385cafd82
amirhossein azarafrooz <amirhosseinazarafrooz@gmail.com>
2023-08-02 22:16:10 +0330

    fix : modify categoriesmenu component (modal)

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -6,7 +6,6 @@ import { getLocalData } from '@/lib/localdata';
 const data = await getLocalData();
 const menu = data.categories
 
-console.log( 'last2', menu);
 export const Header =  () => {
     
 
