0042d8514c0e0ae59fae063eef2f50ce334e249f
Pinfinity <bahramiehsan780@gmail.com>
2023-08-02 17:40:48 +0330

    feat : use redux for product card and add to shop

--- components/ShoppingCard.jsx
+++ components/ShoppingCard.jsx
@@ -1,8 +1,12 @@
 "use client";
+import Image from "next/image";
 import { useRef } from "react";
+import { useSelector } from "react-redux";
 
 export const ShoppingCard = () => {
   const modalWrapperRef = useRef();
+  const cart = useSelector((store) => store.cart);
+  console.log(cart);
   const handleCloseClick = (e) => {
     e.preventDefault();
   };
@@ -39,15 +43,61 @@ export const ShoppingCard = () => {
           <span className="sr-only">Close modal</span>
         </button>
       </div>
-      <div>
-        <div className="p-2 h-full font-iransansl">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem,
-            ut?
-          </p>
+      <div className="overflow-y-auto overflow-x-hidden scroll-smooth scrollbar-hide">
+        <div className=" font-iransansl ">
+          {cart.map((c) => {
+            return (
+              <div className="w-full h-full flex py-3 border-b border-b-gray-50 hover:border-b-gray-200">
+                <div className=" w-28 h-full">
+                  <Image
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                    style={{ width: "auto", height: "auto" }}
+                    src={c.images[0]}
+                  ></Image>
+                </div>
+                <div className="w-full h-full">
+                  <p className="text-sm tracking-tight">{c.name}</p>
+                  <div className="flex justify-between items-center mt-5">
+                    <div>
+                      {c.discount ? (
+                        <div className="flex items-center w-full mb-1">
+                          <div className=" bg-snp-highdiscount text-snp-white px-1 rounded-md font-iransans text-sm">
+                            <div className="flex items-center">
+                              <p>{c.discount}</p>
+                              <p>٪</p>
+                            </div>
+                          </div>
+                          <p className="line-through text-xs text-gray-700 mr-2">
+                            {c.price}
+                          </p>
+                        </div>
+                      ) : null}
+                      <div className="w-full mt-auto">
+                        <div className="flex items-center">
+                          <p className="text-snp-black font-iransans text-md">
+                            {c.price}
+                          </p>
+                          <p className=" text-gray-700 mr-1 text-sm font-iransansl">
+                            تومان
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex justify-center items-center  h-full mt-auto py-3 px-6">
+                      <button className="w-6 bg-blue-600">+</button>
+                      <p>{c.count}</p>
+                      <button className="w-6 bg-blue-600">-</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
-      <div className="flex items-center flex-col border-t p-1 mt-auto ">
+      <div className="flex items-center flex-col border-t p-1 mt-auto">
         <div className="w-full text-center bg-snp-lightgray rounded py-1 text-sm mb-1">
           <p className=" font-iransansl"> حداقل سفارش 100٬000 تومان</p>
         </div>
