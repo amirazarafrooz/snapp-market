3a2837328a595d2f6cff4bea519101866700f86b
Pinfinity <samaniehsan780@gmail.com>
2023-08-04 19:46:23 +0330

    feat: add redux-persist

--- redux/store.js
+++ redux/store.js
@@ -1,9 +1,19 @@
-import {configureStore} from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./cartSlice";
 
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const reducer = combineReducers({ cart: cartSlice });
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
-    reducer: {
-        cart: cartSlice
-    }
-})
+  reducer: persistedReducer,
+});
