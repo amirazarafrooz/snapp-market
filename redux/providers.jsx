edd148b35840fc17c248510af7c84d75723026d8
Pinfinity <samaniehsan780@gmail.com>
2023-08-05 01:30:18 +0330

    set loading for persistGate

--- redux/providers.jsx
+++ redux/providers.jsx
@@ -4,13 +4,20 @@ import { Provider } from "react-redux";
 import { store } from "../redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
+import { useEffect, useState } from "react";
 
 const persistor = persistStore(store);
 
 export function Providers({ children }) {
+  const [isLoading  ,setIsLoading] = useState(false);
+  useEffect(()=> {
+    setTimeout(() => {
+      setIsLoading(true)
+    }, 4000);
+  } ,[])
   return (
     <Provider store={store}>
-      <PersistGate persistor={persistor} loading={null}>
+      <PersistGate persistor={persistor} loading={!isLoading && <div>Loading...</div>}>
         {children}
       </PersistGate>
     </Provider>
