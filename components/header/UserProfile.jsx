a336f8763170d93f794f002ef864e5f93b4976da
amirhossein azarafrooz <amirhosseinazarafrooz@gmail.com>
2023-08-08 19:52:47 +0330

    fix: modify portal in userprofile modal component

--- components/header/UserProfile.jsx
+++ components/header/UserProfile.jsx
@@ -1,91 +1,20 @@
 import Link from "next/link";
 import React, { useState, useEffect,useRef } from "react";
 
-export const UserProfile = ({ headerId, clickHandler, showUserProfile }) => {
+export const UserProfile = ({ handlemodalUser }) => {
 
-  const modal = useRef(null);
-
-  useEffect(() => {
-    // Applying on mount
-    if(showUserProfile) {
-      // document.body.style.overflow = "hidden";
-            console.log('makhfi');
-
-          } else {
-            document.body.style.overflow = "visible";
-            console.log('ashkara');
-
-        }
-    // Applying on unmount    
-            return () => {
-                          }
-          }, [clickHandler])
+ 
 
   return (
     <>
-      <div className="relative " ref={modal}>
-        {headerId === "HeaderSc" ? (
-          <div
-            onClick={clickHandler}
-            href={""}
-            className=" flex items-center cursor-pointer laptop:mx-4 h-full"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              width="32"
-              height="32"
-              fill="currentColor"
-              className="ml-[10px]"
-              viewBox="0 0 32 32"
-              style={{ width: 18, height: 18 }}
-            >
-              <defs>
-                <symbol id="UserIcon" viewBox="0 0 32 32">
-                  <path d="M27.313 20.688a15.92 15.92 0 00-6.083-3.817 9.244 9.244 0 003.602-10.367 9.247 9.247 0 00-8.836-6.508 9.246 9.246 0 00-8.832 6.508 9.244 9.244 0 003.602 10.367A16.03 16.03 0 00-.004 32h2.5c0-7.457 6.047-13.5 13.5-13.5 7.457 0 13.5 6.043 13.5 13.5h2.5a15.88 15.88 0 00-4.683-11.313zM15.995 16a6.752 6.752 0 01-6.75-6.75 6.752 6.752 0 016.75-6.75 6.75 6.75 0 110 13.5zm0 0"></path>
-                </symbol>
-              </defs>
-              <use fill="#FFF" href="#UserIcon" xlinkHref="#UserIcon"></use>
-            </svg>
-            <span className="hidden tablet:text-snp-light   tablet:truncate tablet:block tablet:ml-2 font-iransansl tablet:text-xs laptop:w-20 laptop:text-base">
-              پایونیر تیم گروه دوم
-            </span>
-          </div>
-        ) : (
-          <div
-            onClick={clickHandler}
-            className="flex items-center cursor-pointer"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              width="32"
-              height="32"
-              fill="currentColor"
-              className="ml-[10px]"
-              viewBox="0 0 32 32"
-              style={{ width: 18, height: 18 }}
-            >
-              <defs>
-                <symbol id="UserIcon" viewBox="0 0 32 32">
-                  <path d="M27.313 20.688a15.92 15.92 0 00-6.083-3.817 9.244 9.244 0 003.602-10.367 9.247 9.247 0 00-8.836-6.508 9.246 9.246 0 00-8.832 6.508 9.244 9.244 0 003.602 10.367A16.03 16.03 0 00-.004 32h2.5c0-7.457 6.047-13.5 13.5-13.5 7.457 0 13.5 6.043 13.5 13.5h2.5a15.88 15.88 0 00-4.683-11.313zM15.995 16a6.752 6.752 0 01-6.75-6.75 6.752 6.752 0 016.75-6.75 6.75 6.75 0 110 13.5zm0 0"></path>
-                </symbol>
-              </defs>
-              <use fill="#FFF" href="#UserIcon" xlinkHref="#UserIcon"></use>
-            </svg>
-            <span className="hidden tablet:text-snp-light tablet:w-24 tablet:truncate tablet:block tablet:ml-2 font-iransansl ">
-              پایونیر تیم گروه دوم
-            </span>
-          </div>
-        )}
-        {showUserProfile && (
-           <div className="fixed flex justify-center top-0 left-0 right-0 bottom-0 bg-[#0000006e]  z-[201] h-screen " onClick={clickHandler}>
-            <div className="relative flex justify-end top-14 w-144 tablet:w-192 laptop:w-248 desktop:w-300 h-full z-[202]  ">
-          <div className="bg-snp-white shadow-md  tablet:ml-12 w-60 mobile:w-60 h-72  rounded-b-lg z-[202]" >
+      <div className="relative " >
+        
+       
+          <div className="bg-snp-white shadow-md  tablet:ml-12 w-60 mobile:w-60 h-72  rounded-b-lg z-[202]"onClick={handlemodalUser} >
             <div className="flex flex-col justify-center font-iransans p-3 h-[85px] border-b-2 bg-[#FAFAFA] ">
               <span className="text-sm leading-7 ">پایونیر تیم </span>
               <div className="mt-3 text-sm ">
-                <span>اعتبار:</span>{" "}
+                <span>اعتبار:</span>
                 <span className="bg-orange-400 px-2 py-1 rounded-full text-xs text-snp-white">
                   0 تومان
                 </span>
@@ -206,10 +135,8 @@ export const UserProfile = ({ headerId, clickHandler, showUserProfile }) => {
               </Link>
             </div>
           </div>
-          </div>
-          </div>
 
-        )}
+        
       </div>
     </>
   );
