1f74a33344452bd50bb78ef1ba6da9aa2052b3c1
amirhossein azarafrooz <amirhosseinazarafrooz@gmail.com>
2023-08-08 17:32:22 +0330

    fix: cleaning code in  modal and header components

--- components/header/CategoriesMenu.jsx
+++ components/header/CategoriesMenu.jsx
@@ -1,99 +1,80 @@
-'use client'
-// import React, { useState, useEffect } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import megamenu from '../../public/assets/images/MegaMenuBg.png'
-import Button from '../button/Button';
-import { useRef, useEffect } from 'react';
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import megamenu from "../../public/assets/images/MegaMenuBg.png";
+import { useRef, useEffect } from "react";
 
-
-
-export const CategoriesMenu =  ({showToggle, categoryItems, clickHandler}) => {
-  
+export const CategoriesMenu = ({ showToggle, categoryItems, clickHandler }) => {
   const modal = useRef(null);
 
-  // useEffect(() => {
-  //   // Applying on mount
-  //   if(showToggle) {
-  //     // document.body.style.overflow = "hidden";
-  //     console.log('makhfi');
-
-  //         } else {
-  //           document.body.style.overflow = "visible";
-  //           console.log('ashkara');
-
-  //       }
-  //   // Applying on unmount    
-  //           return () => {
-  //                         }
-  //         }, [clickHandler])
-
-
-    return (
-        
-      <div >
-   {/* {(categoryItems) && (  
-     <div className=' hidden tablet:block' onClick={clickHandler}>
-<Button btnStyleparam={'categories'} >دسته‌بندی‌ها</Button>
-</div>
-)} */}
-
-{/* {(categoryItems) && (<div>{categoryItems?.map((item) => (<div className='bg-red-500'>{item.main}</div>)) }</div>)} */}
-        
-        {/* {(!showToggle) && ( */}
-          {/* <div> */}
-     {(categoryItems) && (
-      
-          //  <div className="fixed flex justify-center top-0 left-0 right-0 bottom-0 bg-[#0000006e]  z-[201] h-screen overflow-hidden" onClick={clickHandler}>
-          //   <div className="relative flex justify-end top-56 w-144 tablet:w-192 laptop:w-248 desktop:w-300 h-full z-[202]  ">
-          <div className=' w-full flex justify-center items-start -mt-4  '>
-        <div className='fixed right-0 top-0 z-50 flex flex-col   laptop:flex  laptop:justify-start laptop:items-center laptop: laptop:flex-wrap laptop:gap-0 w-72 h-full bg-snp-white px-6 pt-6 pb-2
-                        laptop:relative laptop:w-248 laptop:shadow-md laptop:h-96 laptop:px-6 laptop:pt-5 laptop:pb-2 laptop:mx-auto laptop:rounded-b-lg laptop:rounded-tl-lg desktop:w-300'>
-            <div className='flex justify-between items-center mb-6  font-iransansb laptop:hidden'> 
-                <h1>دسته‌بندی‌ها</h1>
-                <span>
+  return (
+    <div>
+      {categoryItems && (
+        <div className=" w-full flex justify-center items-start -mt-4  ">
+          <div
+            className="fixed right-0 top-0 z-50 flex flex-col   laptop:flex  laptop:justify-start laptop:items-center laptop: laptop:flex-wrap laptop:gap-0 w-72 h-full bg-snp-white px-6 pt-6 pb-2
+                        laptop:relative laptop:w-248 laptop:shadow-md laptop:h-96 laptop:px-6 laptop:pt-5 laptop:pb-2 laptop:mx-auto laptop:rounded-b-lg laptop:rounded-tl-lg desktop:w-300"
+          >
+            <div className="flex justify-between items-center mb-6  font-iransansb laptop:hidden">
+              <h1>دسته‌بندی‌ها</h1>
+              <span>
                 <svg
-      xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink"
-      width="32"
-      height="32"
-      fill="currentColor"
-      className="close-icon"
-      viewBox="0 0 32 32"
-      style={{ width: 12, height: 12 }}
-    >
-      <defs>
-        <symbol id="CrossIcon" viewBox="0 0 32 32">
-          <path d="M18.828 16L31.414 3.418a2.004 2.004 0 000-2.832 2.004 2.004 0 00-2.832 0L16 13.172 3.418.586a2.004 2.004 0 00-2.832 0 2.004 2.004 0 000 2.832L13.172 16 .586 28.582A2.004 2.004 0 002 32a2.01 2.01 0 001.418-.586L16 18.828l12.582 12.586c.395.39.906.586 1.418.586a2.004 2.004 0 001.414-3.418zm0 0"></path>
-        </symbol>
-      </defs>
-      <use fill="#757575" href="#CrossIcon" xlinkHref="#CrossIcon"></use>
-                 </svg>
-                </span>
-                </div>
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  width="32"
+                  height="32"
+                  fill="currentColor"
+                  className="close-icon"
+                  viewBox="0 0 32 32"
+                  style={{ width: 12, height: 12 }}
+                >
+                  <defs>
+                    <symbol id="CrossIcon" viewBox="0 0 32 32">
+                      <path d="M18.828 16L31.414 3.418a2.004 2.004 0 000-2.832 2.004 2.004 0 00-2.832 0L16 13.172 3.418.586a2.004 2.004 0 00-2.832 0 2.004 2.004 0 000 2.832L13.172 16 .586 28.582A2.004 2.004 0 002 32a2.01 2.01 0 001.418-.586L16 18.828l12.582 12.586c.395.39.906.586 1.418.586a2.004 2.004 0 001.414-3.418zm0 0"></path>
+                    </symbol>
+                  </defs>
+                  <use
+                    fill="#757575"
+                    href="#CrossIcon"
+                    xlinkHref="#CrossIcon"
+                  ></use>
+                </svg>
+              </span>
+            </div>
             {categoryItems
-            .filter((item)=>item.subs.length>0)
-            ?.map((item) => 
-            (   <Link className=' group hover:bg-white laptop:h-1/6   laptop:my-1'  href={`/categories/${item.main}`}>
-                
-                <div className='flex flex-col font-iransans py-3 laptop:py-1 laptop:p-0 laptop:h-16 leading-5'>
+              .filter((item) => item.subs.length > 0)
+              ?.map((item) => (
+                <Link
+                  className=" group hover:bg-white laptop:h-1/6   laptop:my-1"
+                  href={`/categories/${item.main}`}
+                >
+                  <div className="flex flex-col font-iransans py-3 laptop:py-1 laptop:p-0 laptop:h-16 leading-5">
+                    <div
+                      className='flex items-center text-snp-primary text-sm leading-5 before:ml-2 before:text-snp-secondary before:opacity-50 before:content-["•"] before:text-2xl
+                                     before:group-hover:text-snp-primaryh before:group-hover:opacity-100 before:group-hover:scale-125'
+                    >
+                      {item.mainFA}{" "}
+                    </div>
 
-                <div className='flex items-center text-snp-primary text-sm leading-5 before:ml-2 before:text-snp-secondary before:opacity-50 before:content-["•"] before:text-2xl
-                                     before:group-hover:text-snp-primaryh before:group-hover:opacity-100 before:group-hover:scale-125'>{item.mainFA} </div>
-
-                <div className='  truncate block text-ellipsis text-gray-400 text-sm leading-5 pt-1 w-52 laptop:w-44 group-hover:text-snp-lowdiscount'>{item.subs.map((item)=>(<span className=''>{item} ، </span>))} </div>
-
-             </div>
-             </Link>))}
-             <div className='flex items-end justify-end  h-full'><Image alt='mega-menu' width={201} height={143} src={megamenu}></Image></div>
+                    <div className="  truncate block text-ellipsis text-gray-400 text-sm leading-5 pt-1 w-52 laptop:w-44 group-hover:text-snp-lowdiscount">
+                      {item.subs.map((item) => (
+                        <span className="">{item} ، </span>
+                      ))}{" "}
+                    </div>
+                  </div>
+                </Link>
+              ))}
+            <div className="flex items-end justify-end  h-full">
+              <Image
+                alt="mega-menu"
+                width={201}
+                height={143}
+                src={megamenu}
+              ></Image>
+            </div>
+          </div>
         </div>
-        </div>
-        // </div>
-        // </div>
-        )}
-        </div>
-        // )}
-// </div>        
-    );
+      )}
+    </div>
+  );
 };
-
--- components/header/HeaderEx.jsx
+++ components/header/HeaderEx.jsx
@@ -11,29 +11,21 @@ import Button from "../button/Button";
 import { CategoriesMenu } from "./CategoriesMenu";
 import Link from "next/link";
 import { ShoppingCard } from "./ShoppingCard";
-import { Modal } from "./modal/Modal";
-import {CategoriesMenuModal} from './modal/CategoriesMenuModal'
+import { CategoriesMenuModal } from "./modal/CategoriesMenuModal";
 
 export const HeaderEx = ({ categoryItems, searchItem, subCategory }) => {
-
   const [isOpen, setIsOpen] = useState(false);
   const handlemodal = (event) => {
     event.stopPropagation();
-  } 
+  };
   const handleClose = () => {
-    setIsOpen(false)
-  }
-
+    setIsOpen(false);
+  };
 
   const [showUserProfile, setShowUserProfile] = useState(false);
   const userProfileHandler = useCallback(() => {
     setShowUserProfile(!showUserProfile);
   }, [showUserProfile]);
-
-  // const [showcategory, setShowCategory] = useState(false);
-  // const categoryleHandler = useCallback(() => {
-  //   setShowCategory(!showcategory);
-  // }, [showcategory]);
 
   const [showCart, setShowCart] = useState(false);
   const cartHandler = useCallback(() => {
@@ -168,43 +160,18 @@ export const HeaderEx = ({ categoryItems, searchItem, subCategory }) => {
         <div className=" flex justify-center tablet:w-full ">
           <div className="flex  items-start  p-2 w-144 tablet:justify-start  tablet:w-192 laptop:w-248 desktop:w-300">
             {/* Category btn */}
-            <div className="hidden tablet:block">
-              {/* <Button btnStyleparam={"categories"}>دسته‌بندی‌ها</Button> */}
-              {/* <CategoriesMenu
-                showToggle={showcategory}
-                categoryItems={categoryItems}
-                clickHandler={categoryleHandler}
-              /> */}
-            </div>
+            <div className="hidden tablet:block"></div>
 
             <CategoriesMenuModal handleClose={handleClose} isOpen={isOpen}>
-            <CategoriesMenu
-                // showToggle={showcategory}
-                categoryItems={categoryItems}
-                // clickHandler={categoryleHandler}
-              />
-              
-              </CategoriesMenuModal>
-              <div className=' hidden tablet:block' onClick={() => setIsOpen(true)}>
-                <Button btnStyleparam={'categories'} >دسته‌بندی‌ها</Button>
-              </div>
-                
-            {/* modal */}
-            {/* <div className="App">
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <button onClick={() => setIsOpen(true)}>
-                  Click to Open Modal
-                </button>
+              <CategoriesMenu categoryItems={categoryItems} />
+            </CategoriesMenuModal>
+            <div
+              className=" hidden tablet:block"
+              onClick={() => setIsOpen(true)}
+            >
+              <Button btnStyleparam={"categories"}>دسته‌بندی‌ها</Button>
+            </div>
 
-                <Modal handleClose={handleClose} isOpen={isOpen}>
-                  <div className="w-96 bg-orange-200 h-96 " onClick={handlemodal}>
-                  This is Modal Content!
-                  </div>
-                </Modal>
-
-              </header>
-            </div> */}
             {/* Search box */}
             <SearchBar
               device={"mobile"}
@@ -215,66 +182,6 @@ export const HeaderEx = ({ categoryItems, searchItem, subCategory }) => {
           </div>
         </div>
       </div>
-
-      {/* navbar menu */}
-
-      {/* <div className="flex justify-center w-full bg-white fixed bottom-0 sm:hidden">
-        <div className="flex justify-around w-144 h-22 px-2 pt-4 pb-5">
-          <div className="w-14  flex flex-col justify-center items-center font-iransansl">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              color="gray"
-              viewBox="0 0 20 19"
-            >
-              <path
-                fill="#000"
-                fillRule="evenodd"
-                d="M16.502 17.004a.5.5 0 00.5-.5V7.398c0-.058.005-.114.014-.17l-6.44-4.208c-.576-.38-.576-.38-1.148 0l-6.44 4.21c.009.055.014.111.014.17v9.105a.5.5 0 00.5.5h3.5v-6a1 1 0 011-1h4a1 1 0 011 1v6h3.5zm2.5-.5a2.5 2.5 0 01-2.5 2.5h-4.5a1 1 0 01-1-1v-6h-2v6a1 1 0 01-1 1h-4.5a2.5 2.5 0 01-2.5-2.5V8.398c-.978 0-1.375-1.258-.574-1.82l7.853-5.196C10 .139 10 .139 11.723 1.382l7.853 5.197c.801.56.405 1.82-.573 1.82v8.105z"
-              ></path>
-            </svg>
-            <span>خانه</span>
-          </div>
-          <div className="w-18 flex flex-col justify-center items-center font-iransansl">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              width="32"
-              height="32"
-              fill="currentColor"
-              viewBox="0 0 32 32"
-              style={{ width: 20, height: 20 }}
-            >
-              <defs>
-                <symbol id="GridOutlineIcon" viewBox="0 0 32 32">
-                  <path d="M3.875 2.098c-.684.187-1.355.77-1.676 1.449l-.168.36v8.75l.184.374c.234.48.773 1.02 1.254 1.254l.375.184h8.75l.36-.168c.417-.196.976-.7 1.202-1.086.344-.59.348-.652.332-5.133l-.02-4.113-.198-.422a2.866 2.866 0 00-1.317-1.317l-.422-.199-4.156-.011c-3.512-.012-4.21 0-4.5.078m15.426.02a2.797 2.797 0 00-1.602 1.429l-.168.36v8.75l.184.374c.234.48.773 1.02 1.254 1.254l.375.184 4.281.015c4.77.02 4.57.036 5.242-.41.399-.261.828-.816.988-1.277.106-.305.114-.621.114-4.578v-4.25l-.2-.422a2.866 2.866 0 00-1.316-1.317l-.422-.199-4.187-.011c-3.907-.016-4.211-.008-4.543.097M12.16 4.34l.152.156v3.77c0 3.683-.004 3.777-.126 3.906-.19.207-.285.21-4.14.191l-3.606-.02-.144-.14-.14-.144-.02-3.606c-.02-3.812-.012-3.953.18-4.144.117-.118.242-.122 3.906-.122h3.78l.157.153m15.5 0l.152.156v3.777c0 3.625-.003 3.782-.12 3.922l-.118.149H19.94l-.144-.14-.14-.145-.02-3.606c-.02-3.812-.012-3.953.18-4.144.117-.118.242-.122 3.906-.122h3.78l.157.153M3.8 17.617a2.797 2.797 0 00-1.6 1.43l-.169.36v8.75l.184.374c.234.48.773 1.02 1.254 1.254l.375.184h8.75l.36-.168c.417-.196.976-.7 1.202-1.086.344-.59.348-.652.332-5.133l-.02-4.113-.198-.422a2.866 2.866 0 00-1.317-1.317l-.422-.199-4.187-.011c-3.906-.016-4.211-.008-4.543.097m15.5 0a2.797 2.797 0 00-1.602 1.43l-.168.36v8.75l.184.374c.234.48.773 1.02 1.254 1.254l.375.184 4.281.015c4.77.02 4.57.036 5.242-.41.399-.262.828-.816.988-1.277.106-.305.114-.621.114-4.578v-4.25l-.2-.422a2.866 2.866 0 00-1.316-1.317l-.422-.199-4.187-.011c-3.907-.016-4.211-.008-4.543.097m-7.14 2.223l.152.156v3.77c0 3.683-.004 3.777-.126 3.906-.19.207-.285.21-4.14.191l-3.606-.02-.144-.14-.14-.144-.02-3.606c-.02-3.812-.012-3.953.18-4.144.117-.118.242-.122 3.906-.122h3.78l.157.153m15.5 0l.152.156v3.777c0 3.625-.003 3.782-.12 3.922l-.118.149h-7.633l-.144-.14-.14-.145-.02-3.606c-.02-3.812-.012-3.953.18-4.144.117-.118.242-.122 3.906-.122h3.78l.157.153"></path>
-                </symbol>
-              </defs>
-              <use href="#GridOutlineIcon" xlinkHref="#GridOutlineIcon"></use>
-            </svg>
-            <span>دسته بندی ها</span>
-          </div>
-
-          <div className="w-16 flex flex-col justify-center items-center font-iransansl">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-            >
-              <g fill="none" fillRule="evenodd">
-                <path fill="none" d="M0 0h24v24H0z"></path>
-                <path
-                  fill="#202332"
-                  d="M10 23a2 2 0 11-.001-3.999A2 2 0 0110 23zm7 0a2 2 0 11-.001-3.999A2 2 0 0117 23zM4.208 4H2a1 1 0 010-2h3a1 1 0 01.973.771L6.733 6H21a1 1 0 01.977 1.212l-1.596 7.373A2.994 2.994 0 0117.4 17H9.7c-1.454.028-2.718-.99-2.993-2.381L4.208 4zm2.995 4l1.458 6.195c.094.475.516.814 1.019.805h7.74c.483.01.905-.33 1.003-.822L19.76 8H7.203z"
-                ></path>
-              </g>
-            </svg>
-            <span>سبد خرید</span>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
