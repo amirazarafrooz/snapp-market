5bffbbbe26e3211d3ec226cc4cf6f72773c8450d
Pinfinity <samaniehsan780@gmail.com>
2023-08-11 17:49:28 +0330

    delete unuseful loadings

--- app/(mainlayout)/categories/[categoriesId]/Loading.js
+++
@@ -1,9 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="w-screen h-screen flex justify-center items-center ">
-      <div class="bg-gray-300 p-2  w-5 h-5 rounded-full mx-2 animate-ping duration-200"></div>
-      <div class="bg-gray-400 p-2  w-5 h-5 rounded-full mx-2 animate-ping duration-500"></div>
-      <div class="bg-gray-300 p-2  w-5 h-5 rounded-full mx-2 animate-ping duration-200"></div>
-    </div>
-  );
-}
