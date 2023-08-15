a26bd04bbb60806d93f85c6bccde54149289ec55
Pinfinity <samaniehsan780@gmail.com>
2023-08-15 17:31:57 +0330

    fix filter profit price

--- utils/profitShoppingCardPrice.js
+++ utils/profitShoppingCardPrice.js
@@ -1,9 +1,7 @@
-import { efarsi } from "./efarsi";
-
 export const profitShoppingCardPrice = (cart) => {
   const profitPrice = cart.reduce(
     (init, cur) => (init += cur.count * ((cur.price * cur.discount) / 100)),
     0
   );
-  return efarsi(profitPrice);
+  return profitPrice;
 };
